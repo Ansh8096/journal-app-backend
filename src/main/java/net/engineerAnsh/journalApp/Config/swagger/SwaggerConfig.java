df6abeaf25fdad955d39f72af42c389af646aa28
Ansh8096 <anshv8096@gmail.com>
2026-09-13 01:13:41 +0530

    fix: remove hardcoded swagger server urls

--- src/main/java/net/engineerAnsh/journalApp/Config/swagger/SwaggerConfig.java
+++ src/main/java/net/engineerAnsh/journalApp/Config/swagger/SwaggerConfig.java
@@ -5,33 +5,36 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customConfig() {
-        return new OpenAPI()
-                .info(new Info().
-                        title("Journal APP API's")
-                        .description("By Ansh")
-                ).servers(List.of(
-                        new Server().url("http://localhost:8080").description("Local Server"),
-                        new Server().url("https://journalapp-p6ur.onrender.com").description("Production Server"))
-                ).addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new Components().addSecuritySchemes("bearerAuth",
-                        new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")
-                                .in(SecurityScheme.In.HEADER)
-                                .name("Authorization")
-                ));
 
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Journal APP API's")
+                                .description("By Ansh")
+                )
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList("bearerAuth")
+                )
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        "bearerAuth",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                                .in(SecurityScheme.In.HEADER)
+                                                .name("Authorization")
+                                )
+                );
     }
 }
